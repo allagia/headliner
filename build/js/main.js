611277d0ac7acb08edb4d0d8015ae266a6a22d1b
Alla Panova <alla.mixalvna@gmail.com>
2020-09-28 11:57:34 +0300

    добавляет новые секции

--- build/js/main.js
+++ build/js/main.js
@@ -86,26 +86,12 @@ var maskOptions = {
 (function () {
 
   var swiper = new Swiper('.slider__container', {
-    // autoHeight: true,
     spaceBetween: '50%',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
     },
-    // breakpoints: {
-    //   1281: {
-    //     spaceBetween: 140
-    //   },
-
-    //   768: {
-    //     spaceBetween: 125
-    //   },
-
-    //   320: {
-    //     spaceBetween: 40
-    //   }
-    // }
   });
 })();
 
--- source/js/components/slider.js
+++ source/js/components/slider.js
@@ -3,25 +3,11 @@
 (function () {
 
   var swiper = new Swiper('.slider__container', {
-    // autoHeight: true,
     spaceBetween: '50%',
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
       clickable: true
     },
-    // breakpoints: {
-    //   1281: {
-    //     spaceBetween: 140
-    //   },
-
-    //   768: {
-    //     spaceBetween: 125
-    //   },
-
-    //   320: {
-    //     spaceBetween: 40
-    //   }
-    // }
   });
 })();
